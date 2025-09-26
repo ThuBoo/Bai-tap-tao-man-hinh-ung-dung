6277038136bf950463f93932ca08e22a828b0b46
ThuBoo <tn6642455@gmail.com>
2025-09-26 23:01:33 +0700

    update ForgetPasswordActivity and refactor code for ui

--- app/src/main/java/com/example/myapplication/ForgetPasswordActivity.java
+++ app/src/main/java/com/example/myapplication/ForgetPasswordActivity.java
@@ -3,13 +3,8 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import com.example.myapplication.databinding.ActivityEnterNewPasswordBinding;
 import com.example.myapplication.databinding.ForgetPasswordMainBinding;
 
 public class ForgetPasswordActivity extends AppCompatActivity {
